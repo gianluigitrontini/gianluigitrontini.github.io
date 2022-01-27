113a85d08ee9fbf74e7da328d7b5ef561a541580
Gianluigi Trontini <gianluigitrontini@gmail.com>
2022-01-27 13:28:04 +0100

    Update Project page

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -8,12 +8,15 @@ import remarkGfm from 'remark-gfm';
 import { CloseIcon, OpenLinkIcon } from '../components/utils/Icon';
 import { projects } from '../data';
 
-function Project() {
+function Project2() {
   let { id } = useParams();
   const filteredProject = projects.filter((project) => project.slug === id);
 
   const [project, setProject] = useState(...filteredProject);
-  const [projectDescription, setProjectDescription] = useState('');
+  const [projectDescription, setProjectDescription] = useState({
+    description: null,
+    error: null,
+  });
   const [readingBarWidth, setReadingBarWidth] = useState(0);
 
   const currentProjectIndex = projects.findIndex((prj) => prj.slug === id);
@@ -23,24 +26,28 @@ function Project() {
 
   const getMarkDownDescription = () => {
     fetch(
-      `https://raw.githubusercontent.com/gianluigitrontini/${filteredProject[0].repoName}/master/README.md`
-    )
-      .then((response) =>
-        response.status === 200
-          ? response.text()
-          : 'There may be a problem related to Github that does not allow us to retrieve the description. Please, try again later or visit Github.'
-      )
-      .then((data) => setProjectDescription(data))
-      .catch(
-        'There may be a problem related to Github that does not allow us to retrieve the description.'
-      );
+      `https://raw.githubusercontent.com/gianluigitrontini/${project.repoName}/master/README.md`
+    ).then((response) =>
+      response.status === 200
+        ? response.text().then((data) =>
+            setProjectDescription({
+              description: data,
+              error: null,
+            })
+          )
+        : setProjectDescription({
+            description: null,
+            error:
+              'There may be a problem related to Github that does not allow us to retrieve the description. Please, try again later or visit Github.',
+          })
+    );
   };
 
   useEffect(() => {
     if (project) {
       getMarkDownDescription();
     }
-  }, [filteredProject, project]);
+  }, [project]);
 
   // Feeds the Scrolling indicator
   useEffect(() => {
@@ -57,76 +64,113 @@ function Project() {
 
   if (project) {
     return (
-      <section className='container sm py-4'>
-        <div
-          className='fixed top-0 left-0 right-0 h-1 bg-blue-200'
-          style={{ width: `${readingBarWidth}%` }}></div>
-        <HashLink
-          to={'/#projects'}
-          className='hidden lg:block lg:fixed top-4 right-8'>
-          <span className='close-animation-wrapper absolute top-4 right-8 hover:text-[#38a7ca]'>
-            <span className='close-animation '></span>
-            <CloseIcon />
-          </span>
-        </HashLink>
-
-        <main className='pb-36 lg:pb-8'>
-          <div className='flex flex-col md:flex-row gap-8 items-center bg-gray-50 p-4 rounded-lg'>
-            {project.logo && (
-              <img
-                src={project.logo}
-                className='mx-auto lg:ml-0 w-full max-w-[16rem] h-28 object-contain'
-                alt=''
-              />
+      <section className='flex flex-col lg:flex-row p-0 overflow-hidden'>
+        <div className='lg:hidden flex flex-col gap-8 bg-gray-50 mb-4 px-4 py-8'>
+          {project.logo && (
+            <img
+              src={project.logo}
+              className='mx-auto lg:ml-0 max-w-[12rem] h-20 object-contain'
+              alt=''
+            />
+          )}
+          <div className='text-left font-bold w-full'>
+            {project.linkToProject ? (
+              <a
+                href={project.linkToProject}
+                className='inline-flex items-center text-xl'
+                target='_blank'
+                rel='noreferrer'>
+                {project.name}
+                <OpenLinkIcon />
+              </a>
+            ) : (
+              <h3 className='text-xl'>{project.name}</h3>
             )}
-            <div className='text-left font-bold w-full'>
-              {project.linkToProject ? (
-                <a
-                  href={project.linkToProject}
-                  className='inline-flex items-center text-xl'
-                  target='_blank'
-                  rel='noreferrer'>
-                  {project.name}
-                  <OpenLinkIcon />
-                </a>
-              ) : (
-                <h3 className='text-xl'>{project.name}</h3>
+            <p className=' font-body text-gray-500'>{project.description}</p>
+          </div>
+        </div>
+        <div className='lg:w-2/3 h-full overflow-auto lg:mr-[33%]'>
+          <div className='container sm'>
+            <div
+              className='fixed top-0 left-0 right-0 z-10 h-1 bg-blue-200'
+              style={{ width: `${readingBarWidth}%` }}></div>
+            <HashLink
+              to={'/#projects'}
+              className='hidden lg:block lg:fixed top-4 left-4'>
+              <span className='close-animation-wrapper hover:text-[#38a7ca]'>
+                <span className='close-animation '></span>
+                <CloseIcon />
+              </span>
+            </HashLink>
+            <main className=''>
+              <section
+                id='projects-description'
+                className='text-left py-4 lg:py-16'>
+                {projectDescription.error && (
+                  <div className='bg-red-100 p-4'>
+                    {projectDescription.error}
+                  </div>
+                )}
+                {projectDescription.description && (
+                  <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                    {projectDescription.description}
+                  </ReactMarkdown>
+                )}
+              </section>
+            </main>
+          </div>
+        </div>
+
+        <div className='lg:w-1/3 lg:fixed right-0 top-0 bottom-0 flex flex-col'>
+          <div className='flex-1 bg-gray-50 flex flex-col justify-between p-4'>
+            <div className='hidden lg:flex flex-1 flex-col gap-8'>
+              {project.logo && (
+                <img
+                  src={project.logo}
+                  className='mr-auto max-w-[12rem] h-20 object-contain'
+                  alt=''
+                />
               )}
-              <p className=' font-body text-gray-500'>{project.description}</p>
+              <div className='text-left font-bold w-full'>
+                {project.linkToProject ? (
+                  <a
+                    href={project.linkToProject}
+                    className='inline-flex items-center text-xl'
+                    target='_blank'
+                    rel='noreferrer'>
+                    {project.name}
+                    <OpenLinkIcon />
+                  </a>
+                ) : (
+                  <h3 className='text-xl'>{project.name}</h3>
+                )}
+                <p className=' font-body text-gray-500'>
+                  {project.description}
+                </p>
+              </div>
+            </div>
+            <div className='w-full flex flex-col items-center gap-4 pt-12 previous-next-gradient'>
+              <Link to={`/projects/${previousProject.slug}`} className='w-full'>
+                <button
+                  className='button btn-outline w-full'
+                  onClick={() => setProject(previousProject)}>
+                  &#8592; {previousProject.name}
+                </button>
+              </Link>
+              <Link to={`/projects/${nextProject.slug}`} className='w-full'>
+                <button
+                  className='button w-full'
+                  onClick={() => setProject(nextProject)}>
+                  {nextProject.name} &#8594;
+                </button>
+              </Link>
+              <HashLink to={'/#projects'} className='block lg:hidden'>
+                <span className='my-4 block uppercase underline'>
+                  Back to Homepage
+                </span>
+              </HashLink>
             </div>
           </div>
-
-          <section id='projects-description' className='text-left'>
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
-              {projectDescription}
-            </ReactMarkdown>
-          </section>
-        </main>
-
-        <div className='max-w-[800px] flex flex-col lg:flex-row h-[240px] lg:h-[140px] items-center justify-end lg:justify-between fixed bottom-0 left-[1rem] right-[1rem] mx-auto pt-12 previous-next-gradient'>
-          <Link
-            to={`/projects/${previousProject.slug}`}
-            className='w-full lg:w-auto'>
-            <button
-              className='button btn-outline my-2 lg:my-4 w-full lg:w-auto'
-              onClick={() => setProject(previousProject)}>
-              &#8592; {previousProject.name}
-            </button>
-          </Link>
-          <Link
-            to={`/projects/${nextProject.slug}`}
-            className='w-full lg:w-auto'>
-            <button
-              className='button my-2 lg:my-4 w-full lg:w-auto'
-              onClick={() => setProject(nextProject)}>
-              {nextProject.name} &#8594;
-            </button>
-          </Link>
-          <HashLink to={'/#projects'} className='block lg:hidden'>
-            <span className='my-4 block uppercase underline'>
-              Back to Homepage
-            </span>
-          </HashLink>
         </div>
       </section>
     );
@@ -143,4 +187,4 @@ function Project() {
     );
 }
 
-export default Project;
+export default Project2;
