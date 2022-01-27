5447e18ec5d1fd7ae62e4e52d4100b4c31ffab86
Gianluigi Trontini <gianluigitrontini@gmail.com>
2022-01-27 11:58:54 +0100

    Add Markdown support, fetch descriptions from Github

---
+++ src/components/utils/Icon.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const OpenLinkIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='h-5 w-5 ml-2'
+    fill='none'
+    viewBox='0 0 24 24'
+    stroke='#38a7ca'>
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth={2}
+      d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
+    />
+  </svg>
+);
+
+const CloseIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='h-6 w-6'
+    fill='none'
+    viewBox='0 0 24 24'
+    stroke='currentColor'>
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth={2}
+      d='M6 18L18 6M6 6l12 12'
+    />
+  </svg>
+);
+
+export { OpenLinkIcon, CloseIcon };
--- src/data.js
+++ src/data.js
@@ -8,11 +8,15 @@ import ConnectorLogo from './assets/images/projects/logos/connector-logo.png';
 import VerifyLogo from './assets/images/projects/logos/verify-logo.png';
 import Connector from './assets/images/projects/connector-banner.png';
 import Verify from './assets/images/projects/verify-banner.png';
+import DaniloWeb from './assets/images/projects/danilo-web-banner.png';
+import QuietGuyWeb from './assets/images/projects/quietguy-web-banner.png';
+import InstagramClone from './assets/images/projects/instagram-clone-banner.png';
 
 const projects = [
   {
     name: 'Carousel Learning',
-    slug: 'carousel',
+    slug: 'carousel-learning',
+    repoName: 'carousel-learning',
     description:
       'Carousel is an online tool that helps students to embed knowledge in their long-term memory.',
     image: Carousel,
@@ -24,6 +28,7 @@ const projects = [
   {
     name: 'EntreLibros',
     slug: 'entrelibros',
+    repoName: 'entrelibros',
     description:
       'Find new adventures on paper; Recommend your favorite book to your friends; Do you remember that one they told you about? All this... and more!',
     image: Entrelibros,
@@ -35,6 +40,7 @@ const projects = [
   {
     name: 'RiseRosaRage',
     slug: 'rise-rosa-rage',
+    repoName: 'rise-rosa-rage',
     description: 'Global Socio-Political & Solidarity Graphics since the 60s.',
     image: RiseRosaRage,
     logo: RiseRosaRageLogo,
@@ -44,6 +50,7 @@ const projects = [
   {
     name: 'Aircury Connector',
     slug: 'connector',
+    repoName: 'connector',
     linkToProject: 'https://www.aircury.com/connector',
     description:
       'Make copies of source datasets and lift and shift that data into your own environment, with zero hassle and zero development time for you!',
@@ -55,12 +62,68 @@ const projects = [
   {
     name: 'Aircury School Verify',
     slug: 'verify',
+    repoName: 'verify',
     linkToProject: 'https://www.aircury.com/verify',
     description:
       "Aircury's school picker offers a fast and easy way for a user to select any English or Welsh school so that you can embed those details accurately in your product's database.",
     image: Verify,
     logo: VerifyLogo,
     status: 'active',
+    longDescription: `<p>Long description of projects is still in progress.</p>`,
+  },
+  {
+    name: 'Danilo Barzaghini - Website',
+    slug: 'danilo-web',
+    repoName: 'danilo-barzaghini-web',
+    linkToProject: 'https://gianluigitrontini.github.io/danilo-barzaghini-web/',
+    description: "Danilo Barzaghini's personal website - project from Fiverr.",
+    image: DaniloWeb,
+    status: 'active',
+    longDescription: `<p>Long description of projects is still in progress.</p>`,
+  },
+  {
+    name: 'The Quiet Guy - Portfolio',
+    slug: 'quietguy-web',
+    repoName: 'thequietguy-web',
+    linkToProject: 'https://gianluigitrontini.github.io/thequietguy-web/',
+    description: "TheQuietGuy's portfolio website - project from Fiverr.",
+    image: QuietGuyWeb,
+    status: 'active',
+    longDescription: `<p>Long description of projects is still in progress.</p>`,
+  },
+  {
+    name: 'Instagram Clone',
+    slug: 'instagram-clone',
+    repoName: 'instagram_reactnative-firebase',
+    linkToProject:
+      'https://github.com/gianluigitrontini/instagram_reactnative-firebase',
+    description: 'Instagram clone built with React Native and Firebase.',
+    image: InstagramClone,
+    status: 'demo',
+    longDescription: `<p>Long description of projects is still in progress.</p>`,
+  },
+  {
+    name: 'ECommerce API',
+    slug: 'ecommerce-api',
+    repoName: 'basic-ecommerce_react-node-mongodb',
+    linkToProject:
+      'https://github.com/gianluigitrontini/basic-ecommerce_react-node-mongodb',
+    description:
+      'Simple implementation of an ecommerce functionality with minor UI styling. Add To Cart functionality using Redux and products API built with Node.Js and a local MongoDB database.',
+    image: QuietGuyWeb,
+    status: 'demo',
+    longDescription: `<p>Long description of projects is still in progress.</p>`,
+  },
+  {
+    name: 'Social Network',
+    slug: 'social-network',
+    repoName: 'social-network_react-mongodb',
+    linkToProject:
+      'https://github.com/gianluigitrontini/social-network_react-mongodb',
+    description:
+      'A kind of Facebook but for University. Just like Facebook was, but in 2021. React + Node + MongoDB.',
+    image: QuietGuyWeb,
+    status: 'demo',
     longDescription: `<p>Long description of projects is still in progress.</p>`,
   },
 ];
