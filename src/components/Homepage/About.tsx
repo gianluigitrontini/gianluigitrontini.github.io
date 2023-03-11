d8f060aca7db13a1cd98b53c3555ac830a6fa5d9
Gianluigi Trontini <gianluigitrontini@gmail.com>
2023-03-11 12:20:00 +0100

    Fix minori a sezione about e tecnologie

--- src/components/Homepage/About.tsx
+++ src/components/Homepage/About.tsx
@@ -4,12 +4,13 @@ import Section from "../global/Section";
 const About = () => {
   return (
     <Section id="about" className="flex items-center">
-      <div className="leading-normal w-3/4 mx-auto text-3xl text-center flex flex-col gap-16">
+      <div className="leading-normal lg:w-3/4 mx-auto text-3xl text-center flex flex-col gap-16">
         <p>
           3+ years of professional experience designing &amp; developing&nbsp;
-          <span className="font-bold">websites</span>,&nbsp;
-          <span className="font-bold">web applications</span> and&nbsp;
-          <span className="font-bold">mobile apps</span>.
+          <span className="font-bold inline-block">websites</span>,&nbsp;
+          <span className="font-bold inline-block">web applications</span>{" "}
+          and&nbsp;
+          <span className="font-bold inline-block">mobile apps</span>.
         </p>
         <p>
           I've been working with&nbsp;
--- src/components/Homepage/Technologies.tsx
+++ src/components/Homepage/Technologies.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import Tippy from "@tippyjs/react";
 import { technologies } from "../../data";
 
 const Technologies = () => {
   return (
-    <div className="technologies my-16">
+    <div className="technologies flex flex-wrap gap-16 justify-center my-16">
       {technologies.map((icon, i) => (
         <Tippy key={i} content={icon.name} placement="top">
           <img src={icon.url} alt={icon.name} />
